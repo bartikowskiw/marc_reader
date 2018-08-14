775629cf4a305421a211132e61c9ea2a29e1ce99
bartikowskiw <bartikowskiw@missouri.edu>
2018-08-14 17:06:18 -0500

    Add nextRaw method.

--- src/MarcReader.php
+++ src/MarcReader.php
@@ -2,16 +2,27 @@
 declare( strict_types = 1 );
 namespace Umlts\MarcReader;
 
+/**
+ * Class MarcReader
+ */
 class MarcReader {
 
     public const SOURCE_FILE = 1;
     public const SOURCE_STRING = 2;
 
     public const RT = "\x1D";
+    public const MAX_RECORD_LENGTH = 99999;
 
     protected $source;
+    protected $pos;
     protected $type;
 
+    /**
+     * Constructor
+     *
+     * @param string $source
+     * @param integer $type
+     */
     function __construct( string $source, int $type = self::SOURCE_FILE ) {
         
         $this->source = $source;
@@ -22,16 +33,58 @@ class MarcReader {
                 // Check if the input file is valid
                 $file_info = new \SplFileInfo( $source );
                 if ( !$file_info->isReadable() ) {
-                    throw new \RuntimeException( 'File "'. $source .'"not readable!' );
+                    throw new \RuntimeException( 'File "'. $source .'" not readable!' );
+                }
+                if ( false === $this->source = fopen( $source, 'rb' ) ) {
+                    throw new \RuntimeException( 'File "'. $source .'" is invalid!' );
                 }
                 break;
             case self::SOURCE_STRING:
-                // Do nothing
+                $this->pos = 0;
                 break;
             default:
                 throw new \InvalidArgumentException( 'Type does not exist!' );
         }
 
+    }
+
+    /**
+     * Get next raw record
+     * 
+     * This function mimics the behavoir of the nextRaw method from the 
+     * File_MARC library.
+     *
+     * @return string|bool
+     *   Returns the next raw record or false if no records are left
+     */
+    public function nextRaw() {
+
+        switch ( $this->type ) {
+
+            case self::SOURCE_FILE:
+                $record = stream_get_line(
+                    $this->source,
+                    self::MAX_RECORD_LENGTH,
+                    self::RT
+                );
+                $record .= self::RT;
+                break;
+
+            case self::SOURCE_STRING:
+                $length = strpos( self::RT );
+                if ( $length === false ) { return false; }
+                $record = substr( $this->source, $this->pos, $length );
+                $this->source = substr( $this->source, $length + 1 );
+                $this->pos += $length;
+                break;
+
+            default:
+                throw new \InvalidArgumentException( 'Type does not exist!' );
+        }
+
+        if ( empty( $record ) ) { return false; }
+
+        return $record;
     }
 
     
